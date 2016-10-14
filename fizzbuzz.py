f1f72c025cf8ad4b5064815f2f01795747eb4546
UK-Niki <bull_nicola@hotmail.com>
2016-10-14 11:36:24 +0100

    implement fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,22 @@
+'''
+Fizzbuzz program
+
+Created 14/10/2016
+
+3 = fizz
+5 = buzz
+both = fizz buzz
+'''
+
+print('welcome to Fizzbuzz')
+
+counter = range(1,15,1)
+
+for i in counter:
+	y=i
+	if y % 5 = 0 and y % 3 == 0:
+		print ('FizzBuzz')
+	if y % 5 == 0:
+		print ('Fizz')
+	if y % 3 == 0:
+		print ('Buzz')
